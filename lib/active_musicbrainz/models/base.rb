18420272ad00e3817b7d0c60c90ca0a502cd531f
Andrea Franz <andrea@gravityblast.com>
2013-11-05 03:01:16 +0000

    removed *_name associations

--- lib/active_musicbrainz/models/base.rb
+++ lib/active_musicbrainz/models/base.rb
@@ -6,12 +6,6 @@ module ActiveMusicbrainz
       end
 
       module ClassMethods
-        def has_name association_name
-          define_method :name do
-            send(association_name).name
-          end
-        end
-
         def has_artist_credits
           belongs_to  :artist_credit, foreign_key: :artist_credit
           has_many    :artist_credit_names, through: :artist_credit
--- lib/active_musicbrainz/models/model.rb
+++ lib/active_musicbrainz/models/model.rb
@@ -11,7 +11,6 @@ module ActiveMusicbrainz
 
     Factory.define do
       model :artist do
-        belongs_to  :artist_name, foreign_key: :name
         has_many    :artist_credit_name, foreign_key: :artist
         has_many    :artist_credits, through: :artist_credit_name
         has_many    :release_groups, through: :artist_credits
@@ -19,7 +18,6 @@ module ActiveMusicbrainz
         has_many    :recordings, through: :artist_credits
         has_many    :tracks, through: :artist_credits
         has_many    :aliases, foreign_key: :artist, class_name: 'ArtistAlias'
-        has_name    :artist_name
         has_many    :l_artist_urls, foreign_key: :entity0
         has_many    :urls, through: :l_artist_urls
         has_gid
@@ -32,26 +30,20 @@ module ActiveMusicbrainz
 
       model :artist_credit_name do
         belongs_to  :artist_credit, foreign_key: :artist_credit
-        belongs_to  :artist_name, foreign_key: :name
         belongs_to  :artist, foreign_key: :artist
-        has_name    :artist_name
       end
 
       model :artist_credit do
-        belongs_to  :artist_name, foreign_key: :name
         has_many    :artist_credit_names, foreign_key: :artist_credit
         has_many    :artists, through: :artist_credit_names
         has_many    :recordings, foreign_key: :artist_credit
         has_many    :tracks, foreign_key: :artist_credit
         has_many    :releases, foreign_key: :artist_credit
         has_many    :release_groups, foreign_key: :artist_credit
-        has_name    :artist_name
       end
 
       model :artist_alias do
-        belongs_to  :artist_name, foreign_key: :name
         belongs_to  :artist, foreign_key: :artist
-        has_name    :artist_name
       end
 
       model :url do
@@ -60,11 +52,9 @@ module ActiveMusicbrainz
       end
 
       model :track do
-        belongs_to  :track_name, foreign_key: :name
         belongs_to  :recording, foreign_key: :recording
         belongs_to  :medium, foreign_key: :medium
         has_one     :release, through: :medium
-        has_name    :track_name
         has_artist_credits
         has_gid
       end
@@ -80,9 +70,7 @@ module ActiveMusicbrainz
       end
 
       model :release_group do
-        belongs_to  :release_name, foreign_key: :name
         has_many    :releases, foreign_key: :release_group
-        has_name    :release_name
         has_many    :recordings, through: :releases
         belongs_to  :type, class_name: 'ReleaseGroupPrimaryType', foreign_key: :type
         has_many    :release_group_secondary_type_joins, foreign_key: :release_group
@@ -92,11 +80,9 @@ module ActiveMusicbrainz
       end
 
       model :release do
-        belongs_to  :release_name, foreign_key: :name
         belongs_to  :release_group, foreign_key: :release_group
         has_many    :mediums, -> { order('medium.position') }, foreign_key: :release
         has_many    :recordings, through: :mediums
-        has_name    :release_name
         has_many    :tracks, through: :mediums
         belongs_to  :status, class_name: 'ReleaseStatus', foreign_key: :status
         belongs_to  :packaging, class_name: 'ReleasePackaging', foreign_key: :packaging
@@ -114,13 +100,11 @@ module ActiveMusicbrainz
       end
 
       model :recording do
-        belongs_to  :track_name, foreign_key: :name
         has_many    :tracks, foreign_key: :recording
         has_many    :release_groups, through: :tracks
         has_many    :releases, through: :mediums
         has_many    :release_groups, through: :releases
         has_many    :mediums, through: :tracks
-        has_name    :track_name
         has_artist_credits
         has_gid
       end
