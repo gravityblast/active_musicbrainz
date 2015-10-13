816bd35bc0201f43282859cb4850848e9efab54a
Nicolas Ganz <nicolas@citrin.ch>
2015-10-13 23:44:54 +0200

    added abstract_class=true to the Base class

--- lib/active_musicbrainz/models/base.rb
+++ lib/active_musicbrainz/models/base.rb
@@ -1,6 +1,8 @@
 module ActiveMusicbrainz
   module Model
     class Base < ActiveRecord::Base
+      self.abstract_class = true
+
       def self.has_artist_credits
         belongs_to  :artist_credit, foreign_key: :artist_credit
         has_many    :artist_credit_names, through: :artist_credit
